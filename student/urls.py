05cf92fb2039de77e4c18c1fd0f67c2128ec838a
amjdp <athiramj123@gmail.com>
2023-04-11 23:07:31 +0530

    start project

---
+++ student/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+
+app_name = 'student'
+
+urlpatterns = [
+    path('add_student/',views.add_student, name='add_student'),
+    path('view_student/',views.view_student, name='view_student'),
+    path('update_student',views.update_student, name='update_student'),
+    path('delete_student',views.delete_student, name='delete_student'),
+]
---
+++ student/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def add_student(request):
+    pass
+
+
+def view_student(request):
+    pass
+
+
+def update_student(request):
+    pass
+
+def delete_student(request):
+    pass
+

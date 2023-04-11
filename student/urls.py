66ad4e52dceef038601721ca825bb9d67d3f6d9f
amjdp <athiramj123@gmail.com>
2023-04-11 23:41:02 +0530

    api final

--- student/urls.py
+++ student/urls.py
@@ -6,6 +6,6 @@ app_name = 'student'
 urlpatterns = [
     path('add_student/',views.add_student, name='add_student'),
     path('view_student/',views.view_student, name='view_student'),
-    path('update_student',views.update_student, name='update_student'),
-    path('delete_student',views.delete_student, name='delete_student'),
+    path('update_student/<int:student_id>',views.update_student, name='update_student'),
+    path('delete_student/<int:student_id>',views.delete_student, name='delete_student'),
 ]
